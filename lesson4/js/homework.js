2beeee6d4d0c4b8090c77cff405659cc0747c9e1
Alex <alexmusic8713@gmail.com>
2021-11-29 17:31:43 +0200

    secon commit

--- lesson4/js/homework.js
+++ lesson4/js/homework.js
@@ -14,3 +14,31 @@
 // }
 //
 // sortTheArray(["b", 6, "a", "q", 7, 2])
+
+// --------------------------------------------------------------------------
+
+// 1. 
+let arr1 = [1, 33, 456, 986, 122];
+let result = arr1.reduce((sum, current) => sum + current * current, 0);
+console.log(result);
+
+// 2.
+let arr2 = ['Tanita', 'Vasyl', 'George', 'Stepan'];
+let result2 = arr2.map((item) => `My name is ${item}`);
+console.log(result2);
+
+// 3.
+function sortTheArray(someArray) {
+  const numbArr = someArray.filter((item) => {
+    return typeof item === 'number';
+  });
+  numbArr.sort();
+  const strArr = someArray.filter((item) => {
+    return typeof item === 'string';
+  });
+  strArr.sort();
+  const concatArr = numbArr.concat(strArr);
+  console.log(concatArr);
+}
+
+sortTheArray(["b", 6, "a", "q", 7, 2])
